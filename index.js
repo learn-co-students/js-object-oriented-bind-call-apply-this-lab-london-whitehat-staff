10485258bfde691affcfa9657763840bb8625ca0
Veronika Hunyak <hunyakvera@gmail.com>
2019-03-22 15:18:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //Your code here
+function justInvoke(returnsThisAndArgs) {
+    console.log(window)
+     return returnsThisAndArgs.call(window)
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+    console.log(arguments)
+    console.log(fn.call(thisValue,arg))
+    return fn.call(thisValue,arg)
+}
+
+function setThisWithApply(fn, thisValue, args) {
+    return fn.apply(thisValue, [...args])
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+    return functionToBeCopied.bind(thisValue)
+}
